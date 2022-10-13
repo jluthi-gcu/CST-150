1574dfb0203aa7cbdc11f35fa18b343c25d10f22
jluthi-gcu <113372169+jluthi-gcu@users.noreply.github.com>
2022-10-12 21:29:58 -0700

    Activity 9 Check-In

---
+++ Activity9/MethodSignatures.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity9
+{
+    //Solution: Activity9
+    //Author: Joel Luthi
+    //Date: 10/12/2022
+    //Purpose: Demonstrate writing method signatures.
+    public class MethodSignatures
+    {
+
+        //Problem 1.
+        public void sum(int num1, int num2) {}
+
+        //Problem 2.
+        public double average(double double1, double double2, double double3, double double4, double double5) { return 0.0; }
+
+        //Problem 3.
+        public int randomSum(int random1, int random2) { return 0; }
+
+        //Problem 4.
+        public bool isDivisibleByThree(int num1, int num2, int num3) { return false; }
+
+        //Problem 5.
+        public string fewerCharacterString(string string1, string string2) { return null; }
+
+        //Problem 6.
+        public double largetValue(double[] array) { return 0; }
+
+        //Problem 7.
+        public int[] fiftyIntValues() { return null; }
+
+        //Problem 8.
+        public bool isEqual(bool bool1, bool bool2 ) { return false; }
+
+        //Problem 9.
+        public double product(int value1, double value2) { return 0.0; }
+
+        //Problem 10.
+        public double average(int[][] array) { return 0.0; }
+
+    }
+}
