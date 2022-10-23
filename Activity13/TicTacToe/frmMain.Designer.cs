0deaf56a1bd653c366785c67aad05c23fd6bf35c
jluthi-gcu <113372169+jluthi-gcu@users.noreply.github.com>
2022-10-22 20:03:33 -0700

    Activity 13 Check-In

---
+++ Activity13/TicTacToe/frmMain.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace TicTacToe
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl0_0 = new System.Windows.Forms.Label();
+            this.lbl0_1 = new System.Windows.Forms.Label();
+            this.lbl0_2 = new System.Windows.Forms.Label();
+            this.lbl1_2 = new System.Windows.Forms.Label();
+            this.lbl1_1 = new System.Windows.Forms.Label();
+            this.lbl1_0 = new System.Windows.Forms.Label();
+            this.lbl2_2 = new System.Windows.Forms.Label();
+            this.lbl2_1 = new System.Windows.Forms.Label();
+            this.lbl2_0 = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl0_0
+            // 
+            this.lbl0_0.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl0_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl0_0.ForeColor = System.Drawing.Color.White;
+            this.lbl0_0.Location = new System.Drawing.Point(81, 85);
+            this.lbl0_0.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl0_0.Name = "lbl0_0";
+            this.lbl0_0.Size = new System.Drawing.Size(87, 77);
+            this.lbl0_0.TabIndex = 0;
+            this.lbl0_0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl0_1
+            // 
+            this.lbl0_1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl0_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl0_1.ForeColor = System.Drawing.Color.White;
+            this.lbl0_1.Location = new System.Drawing.Point(193, 85);
+            this.lbl0_1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl0_1.Name = "lbl0_1";
+            this.lbl0_1.Size = new System.Drawing.Size(87, 77);
+            this.lbl0_1.TabIndex = 1;
+            this.lbl0_1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl0_2
+            // 
+            this.lbl0_2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl0_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl0_2.ForeColor = System.Drawing.Color.White;
+            this.lbl0_2.Location = new System.Drawing.Point(305, 85);
+            this.lbl0_2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl0_2.Name = "lbl0_2";
+            this.lbl0_2.Size = new System.Drawing.Size(87, 77);
+            this.lbl0_2.TabIndex = 2;
+            this.lbl0_2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl1_2
+            // 
+            this.lbl1_2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl1_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1_2.ForeColor = System.Drawing.Color.White;
+            this.lbl1_2.Location = new System.Drawing.Point(305, 188);
+            this.lbl1_2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl1_2.Name = "lbl1_2";
+            this.lbl1_2.Size = new System.Drawing.Size(87, 77);
+            this.lbl1_2.TabIndex = 5;
+            this.lbl1_2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl1_1
+            // 
+            this.lbl1_1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl1_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1_1.ForeColor = System.Drawing.Color.White;
+            this.lbl1_1.Location = new System.Drawing.Point(193, 188);
+            this.lbl1_1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl1_1.Name = "lbl1_1";
+            this.lbl1_1.Size = new System.Drawing.Size(87, 77);
+            this.lbl1_1.TabIndex = 4;
+            this.lbl1_1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl1_0
+            // 
+            this.lbl1_0.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl1_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1_0.ForeColor = System.Drawing.Color.White;
+            this.lbl1_0.Location = new System.Drawing.Point(81, 188);
+            this.lbl1_0.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl1_0.Name = "lbl1_0";
+            this.lbl1_0.Size = new System.Drawing.Size(87, 77);
+            this.lbl1_0.TabIndex = 3;
+            this.lbl1_0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl2_2
+            // 
+            this.lbl2_2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl2_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2_2.ForeColor = System.Drawing.Color.White;
+            this.lbl2_2.Location = new System.Drawing.Point(305, 292);
+            this.lbl2_2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl2_2.Name = "lbl2_2";
+            this.lbl2_2.Size = new System.Drawing.Size(87, 77);
+            this.lbl2_2.TabIndex = 8;
+            this.lbl2_2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl2_1
+            // 
+            this.lbl2_1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl2_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2_1.ForeColor = System.Drawing.Color.White;
+            this.lbl2_1.Location = new System.Drawing.Point(193, 292);
+            this.lbl2_1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl2_1.Name = "lbl2_1";
+            this.lbl2_1.Size = new System.Drawing.Size(87, 77);
+            this.lbl2_1.TabIndex = 7;
+            this.lbl2_1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl2_0
+            // 
+            this.lbl2_0.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl2_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2_0.ForeColor = System.Drawing.Color.White;
+            this.lbl2_0.Location = new System.Drawing.Point(81, 292);
+            this.lbl2_0.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl2_0.Name = "lbl2_0";
+            this.lbl2_0.Size = new System.Drawing.Size(87, 77);
+            this.lbl2_0.TabIndex = 6;
+            this.lbl2_0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnNewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewGame.ForeColor = System.Drawing.Color.White;
+            this.btnNewGame.Location = new System.Drawing.Point(73, 404);
+            this.btnNewGame.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(165, 37);
+            this.btnNewGame.TabIndex = 9;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = false;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(247, 404);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(165, 37);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(81, 27);
+            this.lblOutput.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(311, 28);
+            this.lblOutput.TabIndex = 11;
+            this.lblOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(485, 468);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.lbl2_2);
+            this.Controls.Add(this.lbl2_1);
+            this.Controls.Add(this.lbl2_0);
+            this.Controls.Add(this.lbl1_2);
+            this.Controls.Add(this.lbl1_1);
+            this.Controls.Add(this.lbl1_0);
+            this.Controls.Add(this.lbl0_2);
+            this.Controls.Add(this.lbl0_1);
+            this.Controls.Add(this.lbl0_0);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl0_0;
+        private System.Windows.Forms.Label lbl0_1;
+        private System.Windows.Forms.Label lbl0_2;
+        private System.Windows.Forms.Label lbl1_2;
+        private System.Windows.Forms.Label lbl1_1;
+        private System.Windows.Forms.Label lbl1_0;
+        private System.Windows.Forms.Label lbl2_2;
+        private System.Windows.Forms.Label lbl2_1;
+        private System.Windows.Forms.Label lbl2_0;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ Activity13/TicTacToe/frmMain.cs
@@ -0,0 +1,293 @@
+﻿//Solution: TicTacToe
+//Author: Joel Luthi
+//Date: 10/22/2022
+//Purpose: A small game of Tic Tac Toe demonstrating the usage of a 2d array.
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class frmMain : Form
+    {
+        Random _Random = new Random();
+        char[,] _BoardArray = new char[3, 3];
+
+        char _CurrentPlayersTurn = 'X';
+
+        int _TurnsLeft = 9;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            lblOutput.Text = String.Empty;
+
+            ClearBoard();
+
+            //Reset number of turns.
+            _TurnsLeft = 9;
+
+            //Player x always goes first.
+            _CurrentPlayersTurn = 'X';
+
+
+            PlayGame();
+
+
+        }
+
+        public void PlayGame()
+        {
+            //Game loop
+            while (true)
+            {
+                Spot randomSpot = GetRandomAvailableSpot();
+
+                //Place current players mark
+                _BoardArray[randomSpot.Row, randomSpot.Column] = _CurrentPlayersTurn;
+
+                UpdateBoard();
+                //This makes UI changes show.
+                Application.DoEvents();
+
+
+                _TurnsLeft--;
+
+                //Check if current player just won.
+                if (CheckWin())
+                {
+                    lblOutput.Text = $"Player {_CurrentPlayersTurn} Wins!";
+                    break;
+                }
+
+                //Check if turns are over.
+                if (_TurnsLeft == 0)
+                {
+                    lblOutput.Text = "Game Ended in a Tie";
+                    break;
+                }
+
+
+                //Switch players for next turn.
+                if (_CurrentPlayersTurn == 'X')
+                {
+                    _CurrentPlayersTurn = 'O';
+                }
+                else
+                {
+                    _CurrentPlayersTurn = 'X';
+                }
+
+                //Add delay between turns so user can seem game played out on ui.
+                Thread.Sleep(250);
+
+
+            }
+
+
+        }
+
+        //Resets the game board.
+        public void ClearBoard()
+        {
+            for (int row = 0; row < _BoardArray.GetLength(0); row++)
+            {
+                for (int column = 0; column < _BoardArray.GetLength(1); column++)
+                {
+                    _BoardArray[row, column] = ' ';
+                }
+            }
+        }
+
+        //Returns a random 
+        public Spot GetRandomAvailableSpot()
+        {
+            int randomRow = 0;
+            int randomColumn = 0;
+
+            do
+            {
+                //Returns a random number 0 - 2
+                randomRow = _Random.Next(3);
+                randomColumn = _Random.Next(3);
+
+            } while (IsSpotAvailable(randomRow, randomColumn) == false);
+
+            return new Spot(randomRow, randomColumn);
+        }
+
+
+        //Returns if passed in row & column already has a value. Returns false otherwise.
+        public bool IsSpotAvailable(int row, int column)
+        {
+            if (_BoardArray[row, column] == ' ')
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+
+
+        //Updates the UI board with the contents of the 2d array.
+        public void UpdateBoard()
+        {
+            for (int row = 0; row < _BoardArray.GetLength(0); row++)
+            {
+                for (int column = 0; column < _BoardArray.GetLength(1); column++)
+                {
+                    Label lbl = (Label)this.Controls.Find("lbl" + row + "_" + column, false)[0];
+                    lbl.Text = _BoardArray[row, column].ToString();
+                }
+            }
+
+        }
+
+       //Checks if current player won.
+        public bool CheckWin()
+        {
+            char? previousValue = null;
+            bool match = false;
+
+            //Check for row win.
+            for (int row = 0; row < _BoardArray.GetLength(0); row++)
+            {
+                for (int column = 0; column < _BoardArray.GetLength(1); column++)
+                {
+                    if (previousValue == null)
+                    {
+                        previousValue = _BoardArray[row, column];
+                    }
+                    else
+                    {
+                        if (_BoardArray[row, column] != ' ' && _BoardArray[row,column] == previousValue)
+                        {
+                            match = true;
+                        }
+                        else
+                        {
+                            match = false; 
+                            previousValue = null;
+
+                            //Move on to next row
+                            break;
+                        }
+
+                    }
+
+                    //If match and on last cell then this equals win!
+                    if (match && column == _BoardArray.GetLength(1) - 1)
+                    {
+                        return true;
+                    }
+                }
+
+           
+           
+            }
+
+
+            previousValue = null;
+            match = false;
+
+            //Check for column win.
+            for (int column = 0; column < _BoardArray.GetLength(1); column++)
+            {
+                for (int row = 0; row < _BoardArray.GetLength(0); row++)
+                {
+                    if (previousValue == null)
+                    {
+                        previousValue = _BoardArray[row, column];
+                    }
+                    else
+                    {
+                        if (_BoardArray[row, column] != ' ' && _BoardArray[row, column] == previousValue)
+                        {
+                            match = true;
+                        }
+                        else
+                        {
+                            match = false;
+                            previousValue = null;
+                            //Move on to next column
+                            break;
+                        }
+
+                    }
+
+                    //If match and on last cell then this equals win!
+                    if (match && row == _BoardArray.GetLength(0) - 1)
+                    {
+                        return true;
+                    }
+                }
+
+
+            }
+
+
+           
+            //Check for diagonal left to right win.
+            if (_BoardArray[0,0] != ' ')
+            {
+                if (_BoardArray[0, 0] == _BoardArray[1, 1] && _BoardArray[0, 0] == _BoardArray[2, 2])
+                    return true;
+
+            }
+
+            //Check diagonal right to left win.
+            if (_BoardArray[0, 2] != ' ')
+            {
+                if (_BoardArray[0, 2] == _BoardArray[1, 1] && _BoardArray[0, 2] == _BoardArray[2, 0])
+                    return true;
+            }
+
+
+            return false;
+        }
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    //A class to represent the row and column spot of the game board.
+    public class Spot
+    {
+        public int Row { get; set; }
+
+        public int Column { get; set; }
+
+        public Spot(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+    }
+
+
+}
